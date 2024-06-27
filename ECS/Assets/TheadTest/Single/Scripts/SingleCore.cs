ee906b4e90fbc3cb446649e144d2d07aa69756b5
Ultranyan <ultraartgtx@gmail.com>
2024-06-27 13:50:58 +0700

    wtf ?

---
+++ ECS/Assets/TheadTest/Single/Scripts/SingleCore.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleCore : MonoBehaviour
+{
+    public SpawnAndRotate _poolClass;
+    public float rotationSpeed;
+    public int loadCount=100 ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < loadCount; i++)
+        {
+            foreach (var obj in _poolClass.pool)
+            {
+                obj.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+            }
+        }
+        
+    }
+}
---
+++ ECS/Assets/TheadTest/Single/Scripts/SpawnAndRotate.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAndRotate : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefabToSpawn;
+    public int numberOfPrefabs = 1000;
+
+    public  List<Transform> pool;
+    
+    public float spawnRadius = 5f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        pool = new List<Transform>();
+        
+            for (int i = 0; i < numberOfPrefabs; i++)
+            {
+                // Генерируем случайные координаты в пределах spawnRadius
+                Vector2 randomPosition = Random.insideUnitCircle * spawnRadius;
+
+                // Создаем префаб в сгенерированных координатах
+                pool.Add(Instantiate(prefabToSpawn, new Vector3(randomPosition.x, 0f, randomPosition.y), Quaternion.identity).transform);
+
+            }
+        
+        
+    }
+
+}
